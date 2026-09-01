c0fcaa556fc27bd14138b2c4a6334d303a52cb91
latopiafurniture0001-hub <latopiafurniture0001@gmail.com>
2026-09-01 13:01:02 +0700

    Add PK01 API configuration

---
+++ app/src/main/java/com/pk01/android/ApiConfig.kt
@@ -0,0 +1,8 @@
+package com.pk01.android
+
+object ApiConfig {
+    // Example: https://domainanda.com/wp-json/pk01-android/v1/
+    var baseUrl: String = ""
+    var username: String = ""
+    var applicationPassword: String = ""
+}
